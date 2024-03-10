6e42a5bc48c0bea08fddfcda0c93bf8dc0abed56
Ronit Mehta <109467924+Ronit26Mehta@users.noreply.github.com>
2024-03-10 10:52:46 +0530

    Update Election.sol

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+
 pragma solidity >=0.4.21 <0.9.0;
 
 contract Election {
@@ -9,7 +9,7 @@ contract Election {
     bool end;
 
     constructor() public {
-        // Initilizing default values
+
         admin = msg.sender;
         candidateCount = 0;
         voterCount = 0;
@@ -18,16 +18,16 @@ contract Election {
     }
 
     function getAdmin() public view returns (address) {
-        // Returns account address used to deploy contract (i.e. admin)
+        
         return admin;
     }
 
     modifier onlyAdmin() {
-        // Modifier for only admin access
+  
         require(msg.sender == admin);
         _;
     }
-    // Modeling a candidate
+
     struct Candidate {
         uint256 candidateId;
         string header;
@@ -36,10 +36,10 @@ contract Election {
     }
     mapping(uint256 => Candidate) public candidateDetails;
 
-    // Adding new candidates
+   
     function addCandidate(string memory _header, string memory _slogan)
         public
-        // Only admin can add
+    
         onlyAdmin
     {
         Candidate memory newCandidate =
@@ -53,7 +53,7 @@ contract Election {
         candidateCount += 1;
     }
 
-    // Modeling a Election Details
+    
     struct ElectionDetails {
         string adminName;
         string adminEmail;
@@ -71,7 +71,7 @@ contract Election {
         string memory _organizationTitle
     )
         public
-        // Only admin can add
+
         onlyAdmin
     {
         electionDetails = ElectionDetails(
@@ -85,7 +85,7 @@ contract Election {
         end = false;
     }
 
-    // Get Elections details
+
     function getElectionDetails()
     public
     view
@@ -101,19 +101,16 @@ contract Election {
         electionDetails.organizationTitle);
     }
 
-    // Get candidates count
     function getTotalCandidate() public view returns (uint256) {
-        // Returns total number of candidates
         return candidateCount;
     }
 
-    // Get voters count
+ 
     function getTotalVoter() public view returns (uint256) {
-        // Returns total number of voters
         return voterCount;
     }
 
-    // Modeling a voter
+
     struct Voter {
         address voterAddress;
         string name;
@@ -122,10 +119,10 @@ contract Election {
         bool hasVoted;
         bool isRegistered;
     }
-    address[] public voters; // Array of address to store address of voters
+    address[] public voters;
     mapping(address => Voter) public voterDetails;
 
-    // Request to be added as voter
+    
     function registerAsVoter(string memory _name, string memory _phone) public {
         Voter memory newVoter =
             Voter({
@@ -141,16 +138,16 @@ contract Election {
         voterCount += 1;
     }
 
-    // Verify voter
+ 
     function verifyVoter(bool _verifedStatus, address voterAddress)
         public
-        // Only admin can verify
+       
         onlyAdmin
     {
         voterDetails[voterAddress].isVerified = _verifedStatus;
     }
 
-    // Vote
+ 
     function vote(uint256 candidateId) public {
         require(voterDetails[msg.sender].hasVoted == false);
         require(voterDetails[msg.sender].isVerified == true);
@@ -160,13 +157,11 @@ contract Election {
         voterDetails[msg.sender].hasVoted = true;
     }
 
-    // End election
+
     function endElection() public onlyAdmin {
         end = true;
         start = false;
     }
-
-    // Get election start and end values
     function getStart() public view returns (bool) {
         return start;
     }
